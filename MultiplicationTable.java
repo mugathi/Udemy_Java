dcaff68fd73b62571a0079892e7e09f9a901584f
mugathi <mugathipavan333@gmail.com>
2018-05-29 07:25:02 +0530

    MultiplicationTable

---
+++ MultiplicationTable.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class MultiplicationTable
+{
+    public static void main(String[] args) {
+     for(int i=1;i<10;i++)
+     {
+      for (int j=1;j<10 ;j++ ) {
+        System.out.printf("%3d",i*j);
+      }
+       System.out.println(" ");
+     }
+}
+}
