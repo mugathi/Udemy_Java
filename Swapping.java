c99b1e137c78936ee8fc812f9fb4af847006b2e9
mugathi <mugathipavan333@gmail.com>
2018-05-29 07:26:10 +0530

    Swapping

---
+++ Swapping.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Swapping
+{
+    public static void main(String[] args) {
+     int first=10;
+     int second=20;
+     int temp=second;
+     second=first;
+     first=temp;
+     System.out.println("first "+first+" second "+second);
+}
+}
